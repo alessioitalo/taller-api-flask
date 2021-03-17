bc1519048e4f5e5ba077f72bb5b516fbad1393c5
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-03-17 07:04:10 +0000

    wtf form

---
+++ static/index.js
@@ -0,0 +1,14 @@
+//var char1 = document.getElementsByClassName('photo one')[0];
+//var char2 = document.getElementsByClassName('photo two')[0];
+//var credit = document.getElementsByClassName("credit title");
+//
+//
+//if($(char1).is(':hover')){
+//    credit.style.display = 'block'
+//    console.log('test')
+// }
+//
+//if($(char2).is(':hover')){
+//    credit.style.display = 'block'
+// }
+
